c985d82d4747251e2ef16fdb242931c86267020a
Morten Bek Ditlevsen <morten@ka-ching.dk>
2019-09-10 08:38:45 +0200

    Added folder node for configuration

---
+++ Kaching/Kaching.Extensions/FolderProviders/KachingConfigurationFolderProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UCommerce;
+using UCommerce.Tree;
+using UCommerce.Tree.Impl;
+
+namespace Kaching.Extensions.FolderProviders
+{
+    public class KachingConfigurationFolderProvider: ITreeContentProvider
+    {
+        public KachingConfigurationFolderProvider()
+        {
+        }
+
+        public IList<ITreeNodeContent> GetChildren(string nodeType, string id)
+        {
+            return new List<ITreeNodeContent>()
+            {
+                new TreeNodeContent("kaching", -1)
+                {
+                    Name = "Ka-ching",
+                    HasChildren = false,
+                    Action = "/Apps/Ka-ching/contentFiles/any/net452/Configuration.aspx",
+                    Icon = "icon icon-settings"
+                }
+            };
+        }
+
+        public bool Supports(string nodeType)
+        {
+            return nodeType == Constants.DataProvider.NodeType.Settings;
+        }
+    }
+}
---
+++ Kaching/Kaching.Web/Configuration.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>Configuration</title>
+	<script runat="server">
+	
+	</script>
+</head>
+<body>
+	<form id="form1" runat="server">
+	    Yo dawg!
+	</form>
+</body>
+</html>
